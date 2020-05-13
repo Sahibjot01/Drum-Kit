05e9163a17cc66faf2fdf916309e4d5350d2d964
Sahibjot01 <sahibjot70@gmail.com>
2020-05-13 22:10:15 +0530

    completed

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-alert("working");
+// when button is clicked
+var numberOfButtons = document.querySelectorAll(".drum").length;
+for(var i = 0; i<numberOfButtons; i++){
+  document.querySelectorAll("button")[i].addEventListener("click",function(){
+    var button = this.innerHTML;
+    makeSound(button);
+
+  })
+}
+// when key is pressed
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+
+});
+
+function makeSound(key){
+
+  switch (key) {
+    case ("w") :
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        buttonAnimation("w");
+        break;
+    case "a" :
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        buttonAnimation("a");
+        break;
+
+    case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        buttonAnimation("s");
+        break;
+
+    case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        buttonAnimation("d");
+        break;
+
+    case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        buttonAnimation("j");
+        break;
+
+    case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        buttonAnimation("k");
+        break;
+
+    case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        buttonAnimation("l");
+        break;
+    default:
+
+  }
+}
+
+function buttonAnimation(keyPressed){
+  var activeButton = document.querySelector("." + keyPressed);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){activeButton.classList.remove("pressed");},100);
+
+}
